45f522b1ceef6c4c4f580840e5301920fbf7b142
taeuk178 <nous178@naver.com>
2023-03-22 05:33:10 +0900

    controller 적용

--- Sources/App/Controllers/AcronymsController.swift
+++ Sources/App/Controllers/AcronymsController.swift
@@ -15,9 +15,66 @@ struct AcronymsController: RouteCollection {
         let acronymsRoutes = routes.grouped("api", "acronyms")
         acronymsRoutes.get(use: getAllHandler(_:))
         
+        acronymsRoutes.post(use: createHandler(_:))
+        acronymsRoutes.get(":acronymID", use: getHandler(_:))
+        acronymsRoutes.put(":acronymID", use: updateHandler(_:))
+        acronymsRoutes.delete(":acronymID", use: deleteHandler(_:))
+        acronymsRoutes.get("search", use: searchHandler(_:))
+        acronymsRoutes.get("first", use: getFirstHandler(_:))
+        acronymsRoutes.get("sorted", use: sortedHandler(_:))
+        
     }
     
     func getAllHandler(_ req: Request) -> EventLoopFuture<[Acronym]> {
         Acronym.query(on: req.db).all()
     }
+    
+    func createHandler(_ req: Request) throws -> EventLoopFuture<Acronym> {
+        let acronym = try req.content.decode(Acronym.self)
+        return acronym.save(on: req.db).map { acronym }
+    }
+    
+    func getHandler(_ req: Request) -> EventLoopFuture<Acronym> {
+      Acronym.find(req.parameters.get("acronymID"), on: req.db)
+        .unwrap(or: Abort(.notFound))
+    }
+    
+    func updateHandler(_ req: Request) throws -> EventLoopFuture<Acronym> {
+        let updatedAcronym = try req.content.decode(Acronym.self)
+        return Acronym.find(req.parameters.get("acronymID"), on: req.db)
+            .unwrap(or: Abort(.notFound)).flatMap { acronym in
+            acronym.short = updatedAcronym.short
+            acronym.long = updatedAcronym.long
+            return acronym.save(on: req.db).map { acronym }
+        }
+    }
+    
+    func deleteHandler(_ req: Request) -> EventLoopFuture<HTTPStatus> {
+      Acronym.find(req.parameters.get("acronymID"), on: req.db)
+        .unwrap(or: Abort(.notFound))
+        .flatMap { acronym in acronym.delete(on: req.db)
+        .transform(to: .noContent)}
+    }
+    
+    func searchHandler(_ req: Request) throws -> EventLoopFuture<[Acronym]> {
+      guard let searchTerm = req.query[String.self, at: "term"] else {
+          throw Abort(.badRequest)
+      }
+        
+      return Acronym.query(on: req.db).group(.or) { or in
+        or.filter(\.$short == searchTerm)
+        or.filter(\.$long == searchTerm)}
+        .all()
+    }
+    
+    func getFirstHandler(_ req: Request) -> EventLoopFuture<Acronym> {
+      return Acronym.query(on: req.db)
+        .first()
+        .unwrap(or: Abort(.notFound))
+    }
+    
+    func sortedHandler(_ req: Request) -> EventLoopFuture<[Acronym]> {
+      return Acronym.query(on: req.db).sort(\.$short, .ascending).all()
+    }
+    
 }
---
+++ Sources/App/Models/User.swift
@@ -0,0 +1,8 @@
+//
+//  User.swift
+//  
+//
+//  Created by tw on 2023/03/22.
+//
+
+import Foundation
